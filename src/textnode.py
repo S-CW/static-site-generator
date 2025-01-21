0077c4bae28258e909a2a71f55ad144f28fcdcf9
Jeffrey Sing <jeffreysing1188@gmail.com>
2025-01-21 14:09:51 +0800

    initial commit, add textnode class

---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text="", text_type=None, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    
+    def __repr__(self):
+        text_type_value = self.text_type.value if self.text_type else "None"
+        return f"TextNode({self.text}, {text_type_value}, {self.url})"
