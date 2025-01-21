a6c9ebcb50554fa28b0648937e17d5bc16873e81
Jeffrey Sing <jeffreysing1188@gmail.com>
2025-01-21 14:44:16 +0800

    add unit test for TextNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -9,7 +9,7 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode():
-    def __init__(self, text="", text_type=None, url=None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -20,5 +20,4 @@ class TextNode():
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
     
     def __repr__(self):
-        text_type_value = self.text_type.value if self.text_type else "None"
-        return f"TextNode({self.text}, {text_type_value}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
