03e4b9840cd46f21b8cfcb74e950549ca85e2980
Jeffrey Sing <jeffreysing1188@gmail.com>
2025-01-22 14:29:55 +0800

    convert text node to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from util import text_node_to_html
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -28,6 +29,24 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html(node)
+
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"}
         )
 
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from multiprocessing import Value
+
+from src.htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
