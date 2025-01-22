c74aab1a999be1615f98993501f9ed4d712a2bab
Jeffrey Sing <jeffreysing1188@gmail.com>
2025-01-22 16:13:21 +0800

    add inline markdown

--- src/util.py
+++ src/util.py
@@ -1,5 +1,5 @@
 from htmlnode import LeafNode
-from textnode import TextType
+from textnode import TextNode, TextType
 
 
 def text_node_to_html(text_node):
@@ -18,3 +18,26 @@ def text_node_to_html(text_node):
             return LeafNode("img", "", { "src": text_node.url, "alt": text_node.text })
         case _:
             raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+
+        # even value means unclose delimiter
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+        
